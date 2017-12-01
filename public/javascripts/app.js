f437ecb6440f3cbd8246dea48163c21ecfe3fb8b
daronb83 <daronb83@gmail.com>
2017-12-01 02:55:13 +0000

    test page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,24 @@
-var module = angular.module('voter',[]);
+var app = angular.module('voter',[]);
 
-var controller = module.controller('MainCtrl', [
+app.controller('MainCtrl', [
   '$scope',
   function($scope) {
-    $scope.candidate = "Bob Smith";
-    $scope.submission = "Submission";
+
+    $scope.candidates = [];
+    $scope.selected = [];
+      
+    $scope.candidates.push({ name:"Bob Smith", votes:0 });
+    $scope.candidates.push({ name:"Susan Summers", votes:1 });
+
+    $scope.updateValues = function() {
+      $scope.selected = [];
+
+      for (candidate in candidates) {
+        if (candidate.SELECTED === "Y") {
+          $scope.selected.push(candidate);
+        }
+      };
+    };
+
   },
 ]);
